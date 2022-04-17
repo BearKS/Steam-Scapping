ed379221c7ee302effe21751e0926090c9c35e07
Chanismon Thangprakob <flameldrop@gmail.com>
2022-04-17 22:12:16 +0700

    topbar

---
+++ Frontend/src/Topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../Topbar/Topbar.css'
+
+function Topbar() {
+    return(
+         <div className="App-topbar">
+          <div className="log"/>
+          <div className="top-title">MEATS</div>
+        </div>
+    )
+}
+
+export default Topbar
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Topbar from './Topbar/Topbar'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Topbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
