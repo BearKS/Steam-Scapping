6a6054b6470fae4902a995453248e7096d5e13a9
BearKS <56060226+BearKS@users.noreply.github.com>
2022-04-28 01:34:13 +0700

    add logo

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../../Assets/Meats.png'
 export default function Navbar() {
   return ( 
     <div className='container'>
-      <div className='circle'></div>
+      <div className='circle' >
+          <img src = {logo}/>
+      </div>
       <div className='text-logo'>MEATS</div>
     </div>
    );
