df52617fbe23693fef73a12dff48f7b1e697c959
Nawapol <arbruzaz1@gmail.com>
2022-04-28 15:58:55 +0700

    add redirect invalid path

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { NavLink, Route, Routes, Navigate } from "react-router-dom";
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Pages/Home/Home'
-import GamePage from './Pages/Game/GamePage'
-import SearchPage from './Pages/Search/Search'
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import GamePage from "./Pages/Game/GamePage";
+import SearchPage from "./Pages/Search/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-
     <div className="App">
-
       <Navbar />
-      <div >
+      <div>
         <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/Game"} element={<GamePage />} />
-        <Route path={"/Search"} element={<SearchPage />} />
-      </Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path={"/Game"} element={<GamePage />} />
+          <Route path={"/Search"} element={<SearchPage />} />
+        </Routes>
       </div>
-      
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
